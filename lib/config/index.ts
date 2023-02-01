74d4e1b611d534f7f6f0b04e88f7dfb17029dba0
loclink <loclink@163.com>
2023-02-02 02:11:56 +0800

    fix: 修复了打包后版本与生产环境不一致的问题

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -1,5 +1,7 @@
-import ProjectInfo from '/package.json';
-
+import ProjectInfo from '/package.json'
+// import { readJSONSync } from 'fs-extra'
+// import { resolve } from 'path'
+// console.log(readJSONSync(resolve(__dirname, '../package.json')))
 const __VERSION__ = ProjectInfo.version;
 const __COMMAND_NAME__ = Object.keys(ProjectInfo.bin)[0];
 
